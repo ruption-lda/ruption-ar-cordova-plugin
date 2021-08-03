adfd35ed175debcac2b9bd3edd2610f44899b628
ndilokelwa-disrupting <ndilokelwa.luis@ruption.pt>
2021-08-03 10:53:52 +0100

    app compat import

--- src/android/RuptionARPlugin.java
+++ src/android/RuptionARPlugin.java
@@ -47,6 +47,8 @@ import android.view.ViewManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity
+
 import com.google.ar.core.examples.java.helloar.HelloArActivity;
 
 
